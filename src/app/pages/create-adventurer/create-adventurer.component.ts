ad4c8d69dcacbe9596c86caabca7453359a08c64
BARR <ITS_BARRCO@health.gov.au>
2024-02-28 15:17:38 +1100

    updates create page header

--- src/app/pages/create-adventurer/create-adventurer.component.ts
+++ src/app/pages/create-adventurer/create-adventurer.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import {ReactiveFormsModule, FormsModule, FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-create-adventurer',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './create-adventurer.component.html',
   styleUrl: './create-adventurer.component.css'
 })
 export class CreateAdventurerComponent {
+  nameControl : FormControl<string | null> = new FormControl("");
 
 }
