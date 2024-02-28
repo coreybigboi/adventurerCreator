fd64611b92dc0270355d36856c14a644854da6c1
BARR <ITS_BARRCO@health.gov.au>
2024-02-28 13:26:54 +1100

    changed tool bar styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import {RouterLink, RouterOutlet} from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
 import {CreateAdventurerComponent} from "./pages/create-adventurer/create-adventurer.component";
 import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, CreateAdventurerComponent, RouterLink, MatToolbarModule],
+  imports: [RouterOutlet, HomeComponent, CreateAdventurerComponent, RouterLink, MatToolbarModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
