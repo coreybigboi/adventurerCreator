82a386039001152364d5198ed3b6dde86b67cde8
BARR <ITS_BARRCO@health.gov.au>
2024-02-28 14:22:06 +1100

    adds about page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
 import {CreateAdventurerComponent} from "./pages/create-adventurer/create-adventurer.component";
+import {AboutComponent} from "./pages/about/about.component";
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'create', component: CreateAdventurerComponent}
+  {path: 'create', component: CreateAdventurerComponent},
+  {path: 'about', component: AboutComponent}
 ];
