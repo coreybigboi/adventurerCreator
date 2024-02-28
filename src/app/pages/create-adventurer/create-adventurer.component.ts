71099acfe36c5c8ef5fb2adc343d730a0a15a6ba
BARR <ITS_BARRCO@health.gov.au>
2024-02-28 16:10:23 +1100

    adds form group to create page

--- src/app/pages/create-adventurer/create-adventurer.component.ts
+++ src/app/pages/create-adventurer/create-adventurer.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
-import {ReactiveFormsModule, FormsModule, FormControl} from "@angular/forms";
+import {ReactiveFormsModule, FormsModule, FormControl, FormGroup} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
 
 @Component({
   selector: 'app-create-adventurer',
   standalone: true,
-  imports: [ReactiveFormsModule, FormsModule],
+  imports: [ReactiveFormsModule, FormsModule, MatButtonModule, MatInputModule],
   templateUrl: './create-adventurer.component.html',
   styleUrl: './create-adventurer.component.css'
 })
 export class CreateAdventurerComponent {
-  nameControl : FormControl<string | null> = new FormControl("");
 
+  characterform = new FormGroup({
+    nameControl: new FormControl(""),
+    classControl: new FormControl(""),
+    raceControl: new FormControl(""),
+    alignmentControl: new FormControl(""),
+    dobControl: new FormControl(""),
+    backgroundControl: new FormControl(""),
+    equipmentControl: new FormControl(""),
+    goldPiecesControl: new FormControl(""),
+    agreesTermsControl: new FormControl(""),
+  });
 }
